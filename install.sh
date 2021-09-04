9547f6110324170ba6bc8ea777c24952b1c6d220
nrbrt <norklaus@gmail.com>
2021-09-04 05:31:39 +0200

    Update install.sh
    changed back to original avrgirl-arduino module

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir /home/pi/mega;
 cd /home/pi/mega;
-npm install @dawee/avrgirl-arduino;
+npm install avrgirl-arduino;
 wget -O mega2560-dual-smt172-peristalticpump.ino.mega.hex https://raw.githubusercontent.com/nrbrt/2560pro-smt172-peristalticpump/master/mega2560-dual-smt172-peristalticpump.ino.mega.hex;
 wget -O programmega.js https://raw.githubusercontent.com/nrbrt/2560pro-smt172-peristalticpump/master/programmega.js
 node /home/pi/mega/programmega.js;
